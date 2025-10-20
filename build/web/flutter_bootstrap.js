cbcd7cae48cb4e2e576a52cac08c0852ab5c49d7
NihonQuest13 <nathangrondin681@gmail.com>
2025-10-20 09:58:12 +0200

    flutter web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1828717244"
+    serviceWorkerVersion: "3451405377"
   }
 });
